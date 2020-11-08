29941ab133188c9e0f5da30b88304a81bb3db8ac
Tolulope Ayemobola <ayemobolatolulope@gmail.com>
2020-11-08 07:33:46 +0100

    attempted JWT

--- src/main/java/com/socialnetwork/security/ApplicationSecurityConfig.java
+++ src/main/java/com/socialnetwork/security/ApplicationSecurityConfig.java
@@ -1,11 +1,13 @@
 package com.socialnetwork.security;
 
+import com.socialnetwork.jwt.JwtUsernameAndPasswordAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -27,14 +29,22 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http
+                .csrf().disable()
+                .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager()))
+                .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*") // Whitelisting some web pages
                 .permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
                 .anyRequest()
-                .authenticated()
-                .and()
-                .httpBasic();
+                .authenticated();
+//                .and()
+////                .httpBasic();
+//        .formLogin()
+//                .loginPage("/login");
     }
 
     @Override
